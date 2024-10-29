c5cbad32c10eba91527d2e282b43363e01c8bf78
Anonymous User <taylor.koch@su.suffolk.edu>
2024-10-29 12:27:40 -0400

    fix #65 added MassAccess branding and court selection list

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(name='docassemble.MATCTaxLienAnswer',
       url='https://courtformsonline.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.AssemblyLine>=3.0.1'],
+      install_requires=['docassemble.AssemblyLine>=3.1.0'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/MATCTaxLienAnswer/', package='docassemble.MATCTaxLienAnswer'),
      )
